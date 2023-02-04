c57e61da1d475466967ca2795e0fcd1962f9beec
Darnell Andries <dandries@brave.com>
2023-02-04 15:04:12 -0800

    Remove BraveP3A prefix from P3A classes, renamed 'brave' namespace to 'p3a'

--- browser/brave_browser_process.h
+++ browser/brave_browser_process.h
@@ -21,7 +21,6 @@
 
 namespace brave {
 class BraveReferralsService;
-class BraveP3AService;
 class BraveFarblingService;
 class URLSanitizerComponentInstaller;
 }  // namespace brave
@@ -67,6 +66,10 @@ namespace ntp_background_images {
 class NTPBackgroundImagesService;
 }  // namespace ntp_background_images
 
+namespace p3a {
+class P3AService;
+}  // namespace p3a
+
 namespace tor {
 class BraveTorClientUpdater;
 class BraveTorPluggableTransportUpdater;
@@ -111,7 +114,7 @@ class BraveBrowserProcess {
 #if BUILDFLAG(ENABLE_IPFS)
   virtual ipfs::BraveIpfsClientUpdater* ipfs_client_updater() = 0;
 #endif
-  virtual brave::BraveP3AService* brave_p3a_service() = 0;
+  virtual p3a::P3AService* p3a_service() = 0;
   virtual brave::BraveReferralsService* brave_referrals_service() = 0;
   virtual brave_stats::BraveStatsUpdater* brave_stats_updater() = 0;
   virtual ntp_background_images::NTPBackgroundImagesService*
