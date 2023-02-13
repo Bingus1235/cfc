afd8b4d18054538ba6628b0deb8d5f6259278faa
Darnell Andries <dandries@brave.com>
2023-02-13 14:46:10 -0800

    Replace handwritten adblock rust FFI with CXX based FFI

--- components/brave_shields/browser/ad_block_subscription_filters_provider.cc
+++ components/brave_shields/browser/ad_block_subscription_filters_provider.cc
@@ -9,7 +9,7 @@
 
 #include "base/logging.h"
 #include "base/task/thread_pool.h"
-#include "brave/components/adblock_rust_ffi/src/wrapper.h"
+#include "brave/components/brave_shields/adblock/rs/src/lib.rs.h"
 #include "brave/components/brave_shields/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -41,9 +41,13 @@ void AdBlockSubscriptionFiltersProvider::OnDATFileDataReady(
     base::OnceCallback<void(bool deserialize, const DATFileDataBuffer& dat_buf)>
         cb,
     const DATFileDataBuffer& dat_buf) {
-  adblock::FilterListMetadata metadata = adblock::FilterListMetadata(
-      reinterpret_cast<const char*>(dat_buf.data()), dat_buf.size());
-  on_metadata_retrieved_.Run(metadata);
+  auto metadata_result = adblock::read_list_metadata(dat_buf);
+  if (metadata_result.result_kind != adblock::ResultKind::Success) {
+    LOG(ERROR)
+        << "AdBlockSubscriptionFiltersProvider::OnDATFileDataReady failed: "
+        << metadata_result.error_message.c_str();
+  }
+  on_metadata_retrieved_.Run(metadata_result.value);
   std::move(cb).Run(false, dat_buf);
 }
 
