SourabhHere/brave-core

Implement Request Off-The-Record

components/request_otr/browser/request_otr_controller_client.h
@@ -1,0 +1,69 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_REQUEST_OTR_CONTROLLER_CLIENT_H_
+#define BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_REQUEST_OTR_CONTROLLER_CLIENT_H_
+
+#include <memory>
+#include <string>
+
+#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "components/security_interstitials/content/security_interstitial_controller_client.h"
+#include "url/gurl.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+namespace security_interstitials {
+class MetricsHelper;
+}  // namespace security_interstitials
+
+namespace ephemeral_storage {
+class EphemeralStorageService;
+}  // namespace ephemeral_storage
+
+namespace request_otr {
+
+class RequestOTRControllerClient
+    : public security_interstitials::SecurityInterstitialControllerClient {
+ public:
+  static std::unique_ptr<security_interstitials::MetricsHelper>
+  GetMetricsHelper(const GURL& url);
+
+  RequestOTRControllerClient(
+      content::WebContents* web_contents,
+      const GURL& request_url,
+      ephemeral_storage::EphemeralStorageService* ephemeral_storage_service,
+      PrefService* prefs,
+      const std::string& locale);
+  ~RequestOTRControllerClient() override;
+
+  RequestOTRControllerClient(const RequestOTRControllerClient&) = delete;
+  RequestOTRControllerClient& operator=(const RequestOTRControllerClient&) =
+      delete;
+
+  void SetDontWarnAgain(bool value);
+  void ProceedOTR();
+
+  // security_interstitials::SecurityInterstitialControllerClient:
+  void GoBack() override;
+  void Proceed() override;
+
+ private:
+  void ReloadPage();
+
+  const GURL request_url_;
+  bool dont_warn_again_;
+  ephemeral_storage::EphemeralStorageService* ephemeral_storage_service_ =
+      nullptr;  // not owned
+
+  base::WeakPtrFactory<RequestOTRControllerClient> weak_ptr_factory_{this};
+};
+
+}  // namespace request_otr
+
+#endif  // BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_REQUEST_OTR_CONTROLLER_CLIENT_H_

components/request_otr/browser/request_otr_navigation_throttle.h
@@ -1,0 +1,72 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_REQUEST_OTR_NAVIGATION_THROTTLE_H_
+#define BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_REQUEST_OTR_NAVIGATION_THROTTLE_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/memory/weak_ptr.h"
+#include "brave/components/brave_shields/browser/brave_shields_util.h"
+#include "content/public/browser/navigation_throttle.h"
+#include "url/gurl.h"
+
+namespace content {
+class NavigationHandle;
+class WebContents;
+}  // namespace content
+
+namespace ephemeral_storage {
+class EphemeralStorageService;
+}  // namespace ephemeral_storage
+
+namespace request_otr {
+
+class RequestOTRService;
+
+class RequestOTRNavigationThrottle : public content::NavigationThrottle {
+ public:
+  explicit RequestOTRNavigationThrottle(
+      content::NavigationHandle* navigation_handle,
+      RequestOTRService* request_otr_service,
+      ephemeral_storage::EphemeralStorageService* ephemeral_storage_service,
+      const std::string& locale);
+  ~RequestOTRNavigationThrottle() override;
+
+  RequestOTRNavigationThrottle(const RequestOTRNavigationThrottle&) = delete;
+  RequestOTRNavigationThrottle& operator=(const RequestOTRNavigationThrottle&) =
+      delete;
+
+  static std::unique_ptr<RequestOTRNavigationThrottle> MaybeCreateThrottleFor(
+      content::NavigationHandle* navigation_handle,
+      RequestOTRService* request_otr_service,
+      ephemeral_storage::EphemeralStorageService* ephemeral_storage_service,
+      const std::string& locale);
+
+  // content::NavigationThrottle implementation:
+  content::NavigationThrottle::ThrottleCheckResult WillStartRequest() override;
+  content::NavigationThrottle::ThrottleCheckResult WillRedirectRequest()
+      override;
+  content::NavigationThrottle::ThrottleCheckResult WillProcessResponse()
+      override;
+  const char* GetNameForLogging() override;
+
+ private:
+  content::NavigationThrottle::ThrottleCheckResult ShowInterstitial();
+  void Enable1PESAndResume();
+
+  RequestOTRService* request_otr_service_ = nullptr;  // not owned
+  ephemeral_storage::EphemeralStorageService* ephemeral_storage_service_ =
+      nullptr;  // not owned
+  std::string locale_;
+
+  base::WeakPtrFactory<RequestOTRNavigationThrottle> weak_ptr_factory_{this};
+};
+
+}  // namespace request_otr
+
+#endif  // BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_REQUEST_OTR_NAVIGATION_THROTTLE_H_

Bingus1235/cfc

components/request_otr/browser/request_otr_controller_client.h
@@ -1,0 +1,69 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_REQUEST_OTR_BROWSER_REQUEST_OTR_CONTROLLER_CLIENT_H_
+#define BRAVE_COMPONENTS_REQUEST_OTR_BROWSER_REQUEST_OTR_CONTROLLER_CLIENT_H_
+
+#include <memory>
+#include <string>
+
+#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "components/security_interstitials/content/security_interstitial_controller_client.h"
+#include "url/gurl.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+namespace security_interstitials {
+class MetricsHelper;
+}  // namespace security_interstitials
+
+namespace ephemeral_storage {
+class EphemeralStorageService;
+}  // namespace ephemeral_storage
+
+namespace request_otr {
+
+class RequestOTRControllerClient
+    : public security_interstitials::SecurityInterstitialControllerClient {
+ public:
+  static std::unique_ptr<security_interstitials::MetricsHelper>
+  GetMetricsHelper(const GURL& url);
+
+  RequestOTRControllerClient(
+      content::WebContents* web_contents,
+      const GURL& request_url,
+      ephemeral_storage::EphemeralStorageService* ephemeral_storage_service,
+      PrefService* prefs,
+      const std::string& locale);
+  ~RequestOTRControllerClient() override;
+
+  RequestOTRControllerClient(const RequestOTRControllerClient&) = delete;
+  RequestOTRControllerClient& operator=(const RequestOTRControllerClient&) =
+      delete;
+
+  void SetDontWarnAgain(bool value);
+  void ProceedOTR();
+
+  // security_interstitials::SecurityInterstitialControllerClient:
+  void GoBack() override;
+  void Proceed() override;
+
+ private:
+  void ReloadPage();
+
+  const GURL request_url_;
+  bool dont_warn_again_;
+  ephemeral_storage::EphemeralStorageService* ephemeral_storage_service_ =
+      nullptr;  // not owned
+
+  base::WeakPtrFactory<RequestOTRControllerClient> weak_ptr_factory_{this};
+};
+
+}  // namespace request_otr
+
+#endif  // BRAVE_COMPONENTS_REQUEST_OTR_BROWSER_REQUEST_OTR_CONTROLLER_CLIENT_H_

components/request_otr/browser/request_otr_navigation_throttle.h
@@ -1,0 +1,77 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_REQUEST_OTR_BROWSER_REQUEST_OTR_NAVIGATION_THROTTLE_H_
+#define BRAVE_COMPONENTS_REQUEST_OTR_BROWSER_REQUEST_OTR_NAVIGATION_THROTTLE_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/memory/weak_ptr.h"
+#include "brave/components/brave_shields/browser/brave_shields_util.h"
+#include "content/public/browser/navigation_throttle.h"
+#include "url/gurl.h"
+
+class PrefService;
+
+namespace content {
+class NavigationHandle;
+class WebContents;
+}  // namespace content
+
+namespace ephemeral_storage {
+class EphemeralStorageService;
+}  // namespace ephemeral_storage
+
+namespace request_otr {
+
+class RequestOTRService;
+
+class RequestOTRNavigationThrottle : public content::NavigationThrottle {
+ public:
+  explicit RequestOTRNavigationThrottle(
+      content::NavigationHandle* navigation_handle,
+      RequestOTRService* request_otr_service,
+      ephemeral_storage::EphemeralStorageService* ephemeral_storage_service,
+      PrefService* pref_service,
+      const std::string& locale);
+  ~RequestOTRNavigationThrottle() override;
+
+  RequestOTRNavigationThrottle(const RequestOTRNavigationThrottle&) = delete;
+  RequestOTRNavigationThrottle& operator=(const RequestOTRNavigationThrottle&) =
+      delete;
+
+  static std::unique_ptr<RequestOTRNavigationThrottle> MaybeCreateThrottleFor(
+      content::NavigationHandle* navigation_handle,
+      RequestOTRService* request_otr_service,
+      ephemeral_storage::EphemeralStorageService* ephemeral_storage_service,
+      PrefService* pref_service,
+      const std::string& locale);
+
+  // content::NavigationThrottle implementation:
+  content::NavigationThrottle::ThrottleCheckResult WillStartRequest() override;
+  content::NavigationThrottle::ThrottleCheckResult WillRedirectRequest()
+      override;
+  content::NavigationThrottle::ThrottleCheckResult WillProcessResponse()
+      override;
+  const char* GetNameForLogging() override;
+
+ private:
+  content::NavigationThrottle::ThrottleCheckResult MaybeShowInterstitial();
+  void Enable1PESAndResume();
+
+  RequestOTRService* request_otr_service_ = nullptr;  // not owned
+  ephemeral_storage::EphemeralStorageService* ephemeral_storage_service_ =
+      nullptr;                           // not owned
+  PrefService* pref_service_ = nullptr;  // not owned
+  std::string locale_;
+
+  base::WeakPtrFactory<RequestOTRNavigationThrottle> weak_ptr_factory_{this};
+};
+
+}  // namespace request_otr
+
+#endif  // BRAVE_COMPONENTS_REQUEST_OTR_BROWSER_REQUEST_OTR_NAVIGATION_THROTTLE_H_