SourabhHere/brave-core

Add P3A STAR functionality

components/p3a/brave_p3a_metric_log_store.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/p3a/brave_p3a_log_store.h"
+#include "brave/components/p3a/brave_p3a_metric_log_store.h"
 
 #include <vector>
 
@@ -21,8 +21,9 @@
 namespace brave {
 
 namespace {
-constexpr char kTypicalLogPrefName[] = "p3a.logs";
-constexpr char kExpressLogPrefName[] = "p3a.logs_express";
+constexpr char kTypicalJsonLogPrefName[] = "p3a.logs";
+constexpr char kExpressJsonLogPrefName[] = "p3a.logs_express";
+constexpr char kStarPrepPrefName[] = "p3a.logs.star_prep";
 constexpr char kLogValueKey[] = "value";
 constexpr char kLogSentKey[] = "sent";
 constexpr char kLogTimestampKey[] = "timestamp";
@@ -39,15 +40,6 @@ void RecordP3A(uint64_t answers_count) {
   UMA_HISTOGRAM_EXACT_LINEAR("Brave.P3A.SentAnswersCount", answer, 3);
 }
 
-const char* GetPrefName(MetricLogType type) {
-  switch (type) {
-    case MetricLogType::kTypical:
-      return kTypicalLogPrefName;
-    case MetricLogType::kExpress:
-      return kExpressLogPrefName;
-  }
-}
-
 std::string GetUploadType(const std::string& histogram_name) {
   if (base::StartsWith(histogram_name, "Brave.P2A",
                        base::CompareCase::SENSITIVE)) {
@@ -61,23 +53,41 @@ std::string GetUploadType(const std::string& histogram_name) {
 
 }  // namespace
 
-BraveP3ALogStore::BraveP3ALogStore(Delegate* delegate,
-                                   PrefService* local_state,
-                                   MetricLogType type)
-    : delegate_(delegate), local_state_(local_state), type_(type) {
+BraveP3AMetricLogStore::BraveP3AMetricLogStore(Delegate* delegate,
+                                               PrefService* local_state,
+                                               bool is_star,
+                                               MetricLogType type)
+    : delegate_(delegate),
+      local_state_(local_state),
+      type_(type),
+      is_star_(is_star) {
   DCHECK(delegate_);
   DCHECK(local_state);
 }
 
-BraveP3ALogStore::~BraveP3ALogStore() = default;
+BraveP3AMetricLogStore::~BraveP3AMetricLogStore() = default;
 
-void BraveP3ALogStore::RegisterPrefs(PrefRegistrySimple* registry) {
-  registry->RegisterDictionaryPref(kExpressLogPrefName);
-  registry->RegisterDictionaryPref(kTypicalLogPrefName);
+void BraveP3AMetricLogStore::RegisterPrefs(PrefRegistrySimple* registry) {
+  registry->RegisterDictionaryPref(kTypicalJsonLogPrefName);
+  registry->RegisterDictionaryPref(kExpressJsonLogPrefName);
+  registry->RegisterDictionaryPref(kStarPrepPrefName);
 }
 
-void BraveP3ALogStore::UpdateValue(const std::string& histogram_name,
-                                   uint64_t value) {
+const char* BraveP3AMetricLogStore::GetPrefName() const {
+  if (is_star_) {
+    return kStarPrepPrefName;
+  } else {
+    switch (type_) {
+      case MetricLogType::kTypical:
+        return kTypicalJsonLogPrefName;
+      case MetricLogType::kExpress:
+        return kExpressJsonLogPrefName;
+    }
+  }
+}
+
+void BraveP3AMetricLogStore::UpdateValue(const std::string& histogram_name,
+                                         uint64_t value) {
   LogEntry& entry = log_[histogram_name];
   entry.value = value;
 
@@ -87,19 +97,19 @@ void BraveP3ALogStore::UpdateValue(const std::string& histogram_name,
   }
 
   // Update the persistent value.
-  DictionaryPrefUpdate update(local_state_, GetPrefName(type_));
+  DictionaryPrefUpdate update(local_state_, GetPrefName());
   update->SetPath({histogram_name, kLogValueKey},
                   base::Value(base::NumberToString(value)));
   update->SetPath({histogram_name, kLogSentKey}, base::Value(entry.sent));
 }
 
-void BraveP3ALogStore::RemoveValueIfExists(const std::string& histogram_name) {
-  DCHECK(delegate_->IsActualMetric(histogram_name));
+void BraveP3AMetricLogStore::RemoveValueIfExists(
+    const std::string& histogram_name) {
   log_.erase(histogram_name);
   unsent_entries_.erase(histogram_name);
 
   // Update the persistent value.
-  DictionaryPrefUpdate(local_state_, GetPrefName(type_))
+  DictionaryPrefUpdate(local_state_, GetPrefName())
       ->GetDict()
       .Remove(histogram_name);
 
@@ -109,9 +119,9 @@ void BraveP3ALogStore::RemoveValueIfExists(const std::string& histogram_name) {
   }
 }
 
-void BraveP3ALogStore::ResetUploadStamps() {
+void BraveP3AMetricLogStore::ResetUploadStamps() {
   // Clear log entries flags.
-  DictionaryPrefUpdate update(local_state_, GetPrefName(type_));
+  DictionaryPrefUpdate update(local_state_, GetPrefName());
   for (auto& pair : log_) {
     if (pair.second.sent) {
       DCHECK(!pair.second.sent_timestamp.is_null());
@@ -135,19 +145,15 @@ void BraveP3ALogStore::ResetUploadStamps() {
   }
 }
 
-const std::string& BraveP3ALogStore::staged_log_key() const {
-  return staged_entry_key_;
-}
-
-bool BraveP3ALogStore::has_unsent_logs() const {
+bool BraveP3AMetricLogStore::has_unsent_logs() const {
   return !unsent_entries_.empty();
 }
 
-bool BraveP3ALogStore::has_staged_log() const {
+bool BraveP3AMetricLogStore::has_staged_log() const {
   return !staged_entry_key_.empty();
 }
 
-const std::string& BraveP3ALogStore::staged_log() const {
+const std::string& BraveP3AMetricLogStore::staged_log() const {
   DCHECK(!staged_entry_key_.empty());
   auto iter = log_.find(staged_entry_key_);
   DCHECK(iter != log_.end());
@@ -155,7 +161,7 @@ const std::string& BraveP3ALogStore::staged_log() const {
   return staged_log_;
 }
 
-std::string BraveP3ALogStore::staged_log_type() const {
+std::string BraveP3AMetricLogStore::staged_log_type() const {
   DCHECK(!staged_entry_key_.empty());
   auto iter = log_.find(staged_entry_key_);
   DCHECK(iter != log_.end());
@@ -163,22 +169,28 @@ std::string BraveP3ALogStore::staged_log_type() const {
   return GetUploadType(iter->first);
 }
 
-const std::string& BraveP3ALogStore::staged_log_hash() const {
+const std::string& BraveP3AMetricLogStore::staged_log_key() const {
+  DCHECK(!staged_entry_key_.empty());
+
+  return staged_entry_key_;
+}
+
+const std::string& BraveP3AMetricLogStore::staged_log_hash() const {
   NOTREACHED();
   return staged_log_hash_;
 }
 
-const std::string& BraveP3ALogStore::staged_log_signature() const {
+const std::string& BraveP3AMetricLogStore::staged_log_signature() const {
   NOTREACHED();
   return staged_log_signature_;
 }
 
-absl::optional<uint64_t> BraveP3ALogStore::staged_log_user_id() const {
+absl::optional<uint64_t> BraveP3AMetricLogStore::staged_log_user_id() const {
   NOTREACHED();
   return absl::nullopt;
 }
 
-void BraveP3ALogStore::StageNextLog() {
+void BraveP3AMetricLogStore::StageNextLog() {
   // Stage the next item.
   DCHECK(has_unsent_logs());
   uint64_t rand_idx = base::RandGenerator(unsent_entries_.size());
@@ -186,13 +198,15 @@ void BraveP3ALogStore::StageNextLog() {
   DCHECK(!log_.find(staged_entry_key_)->second.sent);
 
   uint64_t staged_entry_value = log_[staged_entry_key_].value;
-  staged_log_ = delegate_->Serialize(staged_entry_key_, staged_entry_value,
-                                     GetUploadType(staged_entry_key_));
+  staged_log_ =
+      delegate_->SerializeLog(staged_entry_key_, staged_entry_value, is_star_,
+                              GetUploadType(staged_entry_key_));
 
-  VLOG(2) << "BraveP3ALogStore::StageNextLog: staged " << staged_entry_key_;
+  VLOG(2) << "BraveP3AMetricLogStore::StageNextLog: staged "
+          << staged_entry_key_;
 }
 
-void BraveP3ALogStore::DiscardStagedLog() {
+void BraveP3AMetricLogStore::DiscardStagedLog() {
   if (!has_staged_log()) {
     return;
   }
@@ -203,7 +217,7 @@ void BraveP3ALogStore::DiscardStagedLog() {
   log_iter->second.MarkAsSent();
 
   // Update the persistent value.
-  DictionaryPrefUpdate update(local_state_, GetPrefName(type_));
+  DictionaryPrefUpdate update(local_state_, GetPrefName());
   update->SetPath({log_iter->first, kLogSentKey},
                   base::Value(log_iter->second.sent));
   update->SetPath({log_iter->first, kLogTimestampKey},
@@ -218,20 +232,20 @@ void BraveP3ALogStore::DiscardStagedLog() {
   staged_log_.clear();
 }
 
-void BraveP3ALogStore::MarkStagedLogAsSent() {}
+void BraveP3AMetricLogStore::MarkStagedLogAsSent() {}
 
-void BraveP3ALogStore::TrimAndPersistUnsentLogs(
+void BraveP3AMetricLogStore::TrimAndPersistUnsentLogs(
     bool overwrite_in_memory_store) {
   NOTREACHED();
 }
 
-void BraveP3ALogStore::LoadPersistedUnsentLogs() {
+void BraveP3AMetricLogStore::LoadPersistedUnsentLogs() {
   DCHECK(log_.empty());
   DCHECK(unsent_entries_.empty());
 
   std::vector<std::string> metrics_to_remove;
 
-  DictionaryPrefUpdate update(local_state_, GetPrefName(type_));
+  DictionaryPrefUpdate update(local_state_, GetPrefName());
   base::Value* list = update.Get();
   for (auto dict_item : list->DictItems()) {
     LogEntry entry;

components/p3a/p3a_message.cc
@@ -5,16 +5,24 @@
 
 #include "brave/components/p3a/p3a_message.h"
 
-#include <vector>
+#include <algorithm>
+#include <array>
 
 #include "base/containers/flat_set.h"
+#include "base/i18n/timezone.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/trace_event/trace_event.h"
+#include "base/strings/string_util.h"
+#include "brave/components/brave_referrals/common/pref_names.h"
+#include "brave/components/brave_stats/browser/brave_stats_updater_util.h"
 #include "brave/components/p3a/brave_p3a_uploader.h"
-#include "crypto/sha2.h"
+#include "brave/components/version_info/version_info.h"
 
 namespace brave {
+
+namespace {
+constexpr std::size_t kP3AStarAttributeCount = 8;
+}  // namespace
 
 MessageMetainfo::MessageMetainfo() = default;
 MessageMetainfo::~MessageMetainfo() = default;
@@ -55,8 +63,66 @@ base::Value::Dict GenerateP3AMessageDict(base::StringPiece metric_name,
   return result;
 }
 
-void MaybeStripRefcodeAndCountry(MessageMetainfo* meta) {
-  const std::string& country = meta->country_code;
+std::string GenerateP3AStarMessage(base::StringPiece metric_name,
+                                   uint64_t metric_value,
+                                   const MessageMetainfo& meta) {
+  base::Time::Exploded exploded;
+  meta.date_of_install.LocalExplode(&exploded);
+  DCHECK_GE(exploded.year, 999);
+
+  std::array<std::array<std::string, 2>, kP3AStarAttributeCount> attributes = {{
+      {"metric_name", std::string(metric_name)},
+      {"metric_value", base::NumberToString(metric_value)},
+      {"version", meta.version},
+      {"yoi", base::NumberToString(exploded.year)},
+      {"channel", meta.channel},
+      {"platform", meta.platform},
+      {"country_code", meta.country_code},
+      {"woi", base::NumberToString(meta.woi)},
+  }};
+
+  std::array<std::string, kP3AStarAttributeCount> serialized_attributes;
+
+  std::transform(attributes.begin(), attributes.end(),
+                 serialized_attributes.begin(), [](auto& attr) -> std::string {
+                   return base::JoinString(attr,
+                                           kP3AMessageStarKeyValueSeparator);
+                 });
+
+  return base::JoinString(serialized_attributes, kP3AMessageStarLayerSeparator);
+}
+
+void MessageMetainfo::Init(PrefService* local_state,
+                           std::string channel,
+                           std::string week_of_install) {
+  platform = brave_stats::GetPlatformIdentifier();
+  this->channel = channel;
+  version = version_info::GetBraveVersionWithoutChromiumMajorVersion();
+
+  if (!week_of_install.empty()) {
+    date_of_install = brave_stats::GetYMDAsDate(week_of_install);
+  } else {
+    date_of_install = base::Time::Now();
+  }
+  woi = brave_stats::GetIsoWeekNumber(date_of_install);
+
+  country_code = base::ToUpperASCII(base::CountryCodeForCurrentTimezone());
+  refcode = local_state->GetString(kReferralPromoCode);
+  MaybeStripRefcodeAndCountry();
+
+  Update();
+
+  VLOG(2) << "Message meta: " << platform << " " << channel << " " << version
+          << " " << woi << " " << wos << " " << country_code << " " << refcode;
+}
+
+void MessageMetainfo::Update() {
+  date_of_survey = base::Time::Now();
+  wos = brave_stats::GetIsoWeekNumber(date_of_survey);
+}
+
+void MessageMetainfo::MaybeStripRefcodeAndCountry() {
+  const std::string& country = country_code;
   constexpr char kRefcodeNone[] = "none";
   constexpr char kCountryOther[] = "other";
 
@@ -68,23 +134,21 @@ void MaybeStripRefcodeAndCountry(MessageMetainfo* meta) {
       {"US", "FR", "PH", "GB", "IN", "DE", "BR", "CA", "IT", "ES", "NL", "MX",
        "AU", "RU", "JP", "PL", "ID", "KR", "AR"});
 
-  DCHECK(meta);
-
   // Always strip the refcode.
   // We no longer need to partition P3A data with that key.
-  meta->refcode = kRefcodeNone;
+  refcode = kRefcodeNone;
 
-  if (meta->platform == "linux-bc") {
+  if (platform == "linux-bc") {
     // If we have more than 3/0.05 = 60 users in a country for
     // a week of install, we can send country.
     if (kLinuxCountries.count(country) == 0) {
-      meta->country_code = kCountryOther;
+      country_code = kCountryOther;
     }
   } else {
     // Now the minimum platform is MacOS at ~3%, so cut off for a group under
     // here becomes 3/(0.05*0.03) = 2000.
     if (kNotableCountries.count(country) == 0) {
-      meta->country_code = kCountryOther;
+      country_code = kCountryOther;
     }
   }
 }

Bingus1235/cfc

components/p3a/brave_p3a_metric_log_store.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/p3a/brave_p3a_log_store.h"
+#include "brave/components/p3a/brave_p3a_metric_log_store.h"
 
 #include <vector>
 
@@ -21,9 +21,10 @@
 namespace brave {
 
 namespace {
-constexpr char kTypicalLogPrefName[] = "p3a.logs";
-constexpr char kExpressLogPrefName[] = "p3a.logs_express";
-constexpr char kSlowLogPrefName[] = "p3a.logs_slow";
+constexpr char kTypicalJsonLogPrefName[] = "p3a.logs";
+constexpr char kSlowJsonLogPrefName[] = "p3a.logs_slow";
+constexpr char kExpressJsonLogPrefName[] = "p3a.logs_express";
+constexpr char kStarPrepPrefName[] = "p3a.logs.star_prep";
 constexpr char kLogValueKey[] = "value";
 constexpr char kLogSentKey[] = "sent";
 constexpr char kLogTimestampKey[] = "timestamp";
@@ -40,17 +41,6 @@ void RecordSentAnswersCount(uint64_t answers_count) {
   UMA_HISTOGRAM_EXACT_LINEAR("Brave.P3A.SentAnswersCount", answer, 3);
 }
 
-const char* GetPrefName(MetricLogType type) {
-  switch (type) {
-    case MetricLogType::kSlow:
-      return kSlowLogPrefName;
-    case MetricLogType::kTypical:
-      return kTypicalLogPrefName;
-    case MetricLogType::kExpress:
-      return kExpressLogPrefName;
-  }
-}
-
 std::string GetUploadType(const std::string& histogram_name) {
   if (base::StartsWith(histogram_name, "Brave.P2A",
                        base::CompareCase::SENSITIVE)) {
@@ -64,24 +54,44 @@ std::string GetUploadType(const std::string& histogram_name) {
 
 }  // namespace
 
-BraveP3ALogStore::BraveP3ALogStore(Delegate* delegate,
-                                   PrefService* local_state,
-                                   MetricLogType type)
-    : delegate_(delegate), local_state_(local_state), type_(type) {
+BraveP3AMetricLogStore::BraveP3AMetricLogStore(Delegate* delegate,
+                                               PrefService* local_state,
+                                               bool is_star,
+                                               MetricLogType type)
+    : delegate_(delegate),
+      local_state_(local_state),
+      type_(type),
+      is_star_(is_star) {
   DCHECK(delegate_);
   DCHECK(local_state);
 }
 
-BraveP3ALogStore::~BraveP3ALogStore() = default;
+BraveP3AMetricLogStore::~BraveP3AMetricLogStore() = default;
 
-void BraveP3ALogStore::RegisterPrefs(PrefRegistrySimple* registry) {
-  registry->RegisterDictionaryPref(kExpressLogPrefName);
-  registry->RegisterDictionaryPref(kTypicalLogPrefName);
-  registry->RegisterDictionaryPref(kSlowLogPrefName);
+void BraveP3AMetricLogStore::RegisterPrefs(PrefRegistrySimple* registry) {
+  registry->RegisterDictionaryPref(kTypicalJsonLogPrefName);
+  registry->RegisterDictionaryPref(kExpressJsonLogPrefName);
+  registry->RegisterDictionaryPref(kSlowJsonLogPrefName);
+  registry->RegisterDictionaryPref(kStarPrepPrefName);
 }
 
-void BraveP3ALogStore::UpdateValue(const std::string& histogram_name,
-                                   uint64_t value) {
+const char* BraveP3AMetricLogStore::GetPrefName() const {
+  if (is_star_) {
+    return kStarPrepPrefName;
+  } else {
+    switch (type_) {
+      case MetricLogType::kTypical:
+        return kTypicalJsonLogPrefName;
+      case MetricLogType::kExpress:
+        return kExpressJsonLogPrefName;
+      case MetricLogType::kSlow:
+        return kSlowJsonLogPrefName;
+    }
+  }
+}
+
+void BraveP3AMetricLogStore::UpdateValue(const std::string& histogram_name,
+                                         uint64_t value) {
   LogEntry& entry = log_[histogram_name];
   entry.value = value;
 
@@ -91,19 +101,19 @@ void BraveP3ALogStore::UpdateValue(const std::string& histogram_name,
   }
 
   // Update the persistent value.
-  ScopedDictPrefUpdate update(local_state_, GetPrefName(type_));
+  ScopedDictPrefUpdate update(local_state_, GetPrefName());
   base::Value::Dict* log_dict = update->EnsureDict(histogram_name);
   log_dict->Set(kLogValueKey, base::NumberToString(value));
   log_dict->Set(kLogSentKey, entry.sent);
 }
 
-void BraveP3ALogStore::RemoveValueIfExists(const std::string& histogram_name) {
-  DCHECK(delegate_->IsActualMetric(histogram_name));
+void BraveP3AMetricLogStore::RemoveValueIfExists(
+    const std::string& histogram_name) {
   log_.erase(histogram_name);
   unsent_entries_.erase(histogram_name);
 
   // Update the persistent value.
-  ScopedDictPrefUpdate(local_state_, GetPrefName(type_))
+  ScopedDictPrefUpdate(local_state_, GetPrefName())
       ->Remove(histogram_name);
 
   if (has_staged_log() && staged_entry_key_ == histogram_name) {
@@ -112,9 +122,9 @@ void BraveP3ALogStore::RemoveValueIfExists(const std::string& histogram_name) {
   }
 }
 
-void BraveP3ALogStore::ResetUploadStamps() {
+void BraveP3AMetricLogStore::ResetUploadStamps() {
   // Clear log entries flags.
-  ScopedDictPrefUpdate update(local_state_, GetPrefName(type_));
+  ScopedDictPrefUpdate update(local_state_, GetPrefName());
   for (auto& pair : log_) {
     if (pair.second.sent) {
       DCHECK(!pair.second.sent_timestamp.is_null());
@@ -141,19 +151,15 @@ void BraveP3ALogStore::ResetUploadStamps() {
   }
 }
 
-const std::string& BraveP3ALogStore::staged_log_key() const {
-  return staged_entry_key_;
-}
-
-bool BraveP3ALogStore::has_unsent_logs() const {
+bool BraveP3AMetricLogStore::has_unsent_logs() const {
   return !unsent_entries_.empty();
 }
 
-bool BraveP3ALogStore::has_staged_log() const {
+bool BraveP3AMetricLogStore::has_staged_log() const {
   return !staged_entry_key_.empty();
 }
 
-const std::string& BraveP3ALogStore::staged_log() const {
+const std::string& BraveP3AMetricLogStore::staged_log() const {
   DCHECK(!staged_entry_key_.empty());
   auto iter = log_.find(staged_entry_key_);
   DCHECK(iter != log_.end());
@@ -161,7 +167,7 @@ const std::string& BraveP3ALogStore::staged_log() const {
   return staged_log_;
 }
 
-std::string BraveP3ALogStore::staged_log_type() const {
+std::string BraveP3AMetricLogStore::staged_log_type() const {
   DCHECK(!staged_entry_key_.empty());
   auto iter = log_.find(staged_entry_key_);
   DCHECK(iter != log_.end());
@@ -169,22 +175,28 @@ std::string BraveP3ALogStore::staged_log_type() const {
   return GetUploadType(iter->first);
 }
 
-const std::string& BraveP3ALogStore::staged_log_hash() const {
+const std::string& BraveP3AMetricLogStore::staged_log_key() const {
+  DCHECK(!staged_entry_key_.empty());
+
+  return staged_entry_key_;
+}
+
+const std::string& BraveP3AMetricLogStore::staged_log_hash() const {
   NOTREACHED();
   return staged_log_hash_;
 }
 
-const std::string& BraveP3ALogStore::staged_log_signature() const {
+const std::string& BraveP3AMetricLogStore::staged_log_signature() const {
   NOTREACHED();
   return staged_log_signature_;
 }
 
-absl::optional<uint64_t> BraveP3ALogStore::staged_log_user_id() const {
+absl::optional<uint64_t> BraveP3AMetricLogStore::staged_log_user_id() const {
   NOTREACHED();
   return absl::nullopt;
 }
 
-void BraveP3ALogStore::StageNextLog() {
+void BraveP3AMetricLogStore::StageNextLog() {
   // Stage the next item.
   DCHECK(has_unsent_logs());
   uint64_t rand_idx = base::RandGenerator(unsent_entries_.size());
@@ -192,13 +204,15 @@ void BraveP3ALogStore::StageNextLog() {
   DCHECK(!log_.find(staged_entry_key_)->second.sent);
 
   uint64_t staged_entry_value = log_[staged_entry_key_].value;
-  staged_log_ = delegate_->Serialize(staged_entry_key_, staged_entry_value,
-                                     type_, GetUploadType(staged_entry_key_));
+  staged_log_ =
+      delegate_->SerializeLog(staged_entry_key_, staged_entry_value, type_, is_star_,
+                              GetUploadType(staged_entry_key_));
 
-  VLOG(2) << "BraveP3ALogStore::StageNextLog: staged " << staged_entry_key_;
+  VLOG(2) << "BraveP3AMetricLogStore::StageNextLog: staged "
+          << staged_entry_key_;
 }
 
-void BraveP3ALogStore::DiscardStagedLog() {
+void BraveP3AMetricLogStore::DiscardStagedLog() {
   if (!has_staged_log()) {
     return;
   }
@@ -209,7 +223,7 @@ void BraveP3ALogStore::DiscardStagedLog() {
   log_iter->second.MarkAsSent();
 
   // Update the persistent value.
-  ScopedDictPrefUpdate update(local_state_, GetPrefName(type_));
+  ScopedDictPrefUpdate update(local_state_, GetPrefName());
   base::Value::Dict* log_dict = update->EnsureDict(log_iter->first);
   log_dict->Set(kLogSentKey, log_iter->second.sent);
   log_dict->Set(kLogTimestampKey, log_iter->second.sent_timestamp.ToDoubleT());
@@ -223,18 +237,18 @@ void BraveP3ALogStore::DiscardStagedLog() {
   staged_log_.clear();
 }
 
-void BraveP3ALogStore::MarkStagedLogAsSent() {}
+void BraveP3AMetricLogStore::MarkStagedLogAsSent() {}
 
-void BraveP3ALogStore::TrimAndPersistUnsentLogs(
+void BraveP3AMetricLogStore::TrimAndPersistUnsentLogs(
     bool overwrite_in_memory_store) {
   NOTREACHED();
 }
 
-void BraveP3ALogStore::LoadPersistedUnsentLogs() {
+void BraveP3AMetricLogStore::LoadPersistedUnsentLogs() {
   DCHECK(log_.empty());
   DCHECK(unsent_entries_.empty());
 
-  const char* pref_name = GetPrefName(type_);
+  const char* pref_name = GetPrefName();
 
   std::vector<std::string> metrics_to_remove;
 

components/p3a/p3a_message.cc
@@ -5,17 +5,24 @@
 
 #include "brave/components/p3a/p3a_message.h"
 
-#include <vector>
+#include <algorithm>
+#include <array>
 
 #include "base/containers/flat_set.h"
+#include "base/i18n/timezone.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/trace_event/trace_event.h"
+#include "base/strings/string_util.h"
+#include "brave/components/brave_referrals/buildflags/buildflags.h"
 #include "brave/components/brave_stats/browser/brave_stats_updater_util.h"
 #include "brave/components/p3a/brave_p3a_uploader.h"
-#include "crypto/sha2.h"
+#include "brave/components/version_info/version_info.h"
 
 namespace brave {
+
+namespace {
+constexpr std::size_t kP3AStarAttributeCount = 8;
+}  // namespace
 
 MessageMetainfo::MessageMetainfo() = default;
 MessageMetainfo::~MessageMetainfo() = default;
@@ -90,8 +97,63 @@ base::Value::Dict GenerateP3AMessageDict(base::StringPiece metric_name,
   return result;
 }
 
-void MaybeStripCountry(MessageMetainfo* meta) {
-  const std::string& country = meta->country_code;
+std::string GenerateP3AStarMessage(base::StringPiece metric_name,
+                                   uint64_t metric_value,
+                                   const MessageMetainfo& meta) {
+  base::Time::Exploded exploded;
+  meta.date_of_install.LocalExplode(&exploded);
+  DCHECK_GE(exploded.year, 999);
+
+  std::array<std::array<std::string, 2>, kP3AStarAttributeCount> attributes = {{
+      {"metric_name", std::string(metric_name)},
+      {"metric_value", base::NumberToString(metric_value)},
+      {"version", meta.version},
+      {"yoi", base::NumberToString(exploded.year)},
+      {"channel", meta.channel},
+      {"platform", meta.platform},
+      {"country_code", meta.country_code},
+      {"woi", base::NumberToString(meta.woi)},
+  }};
+
+  std::array<std::string, kP3AStarAttributeCount> serialized_attributes;
+
+  std::transform(attributes.begin(), attributes.end(),
+                 serialized_attributes.begin(), [](auto& attr) -> std::string {
+                   return base::JoinString(attr,
+                                           kP3AMessageStarKeyValueSeparator);
+                 });
+
+  return base::JoinString(serialized_attributes, kP3AMessageStarLayerSeparator);
+}
+
+void MessageMetainfo::Init(PrefService* local_state,
+                           std::string brave_channel,
+                           std::string week_of_install) {
+  platform = brave_stats::GetPlatformIdentifier();
+  channel = brave_channel;
+  version = version_info::GetBraveVersionWithoutChromiumMajorVersion();
+
+  if (!week_of_install.empty()) {
+    date_of_install = brave_stats::GetYMDAsDate(week_of_install);
+  } else {
+    date_of_install = base::Time::Now();
+  }
+  woi = brave_stats::GetIsoWeekNumber(date_of_install);
+
+  country_code = base::ToUpperASCII(base::CountryCodeForCurrentTimezone());
+  MaybeStripCountry();
+
+  Update();
+
+  VLOG(2) << "Message meta: " << platform << " " << channel << " " << version
+          << " " << woi << " " << country_code;
+}
+
+void MessageMetainfo::Update() {
+  date_of_survey = base::Time::Now();
+}
+
+void MessageMetainfo::MaybeStripCountry() {
   constexpr char kCountryOther[] = "other";
 
   static base::flat_set<std::string> const kLinuxCountries(
@@ -102,19 +164,17 @@ void MaybeStripCountry(MessageMetainfo* meta) {
       {"US", "FR", "PH", "GB", "IN", "DE", "BR", "CA", "IT", "ES", "NL", "MX",
        "AU", "RU", "JP", "PL", "ID", "KR", "AR"});
 
-  DCHECK(meta);
-
-  if (meta->platform == "linux-bc") {
+  if (platform == "linux-bc") {
     // If we have more than 3/0.05 = 60 users in a country for
     // a week of install, we can send country.
-    if (kLinuxCountries.count(country) == 0) {
-      meta->country_code = kCountryOther;
+    if (kLinuxCountries.count(country_code) == 0) {
+      country_code = kCountryOther;
     }
   } else {
     // Now the minimum platform is MacOS at ~3%, so cut off for a group under
     // here becomes 3/(0.05*0.03) = 2000.
-    if (kNotableCountries.count(country) == 0) {
-      meta->country_code = kCountryOther;
+    if (kNotableCountries.count(country_code) == 0) {
+      country_code = kCountryOther;
     }
   }
 }